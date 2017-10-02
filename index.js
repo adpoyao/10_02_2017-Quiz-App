628bc8e489410317c6c253fca38d058dd3bda358
Eddie Yao <adpoyao@gmail.com>
2017-10-02 09:02:49 -0700

    added outline

--- index.js
+++ index.js
@@ -6,5 +6,40 @@ const STORE = {
     {}, {}, {}, {}, {}
   ],
   view: 'intro',
-  
-}
+
+};
+
+//INTRO VIEW: listen for user to click "START" button
+
+//QUESTION VIEW: STORE gets rendered
+
+//Listen for user selections on answers
+
+//Listen for user to "submit" button
+
+/* 
+ALERT: 
+(1)Textual Feedback: Correct or Wrong;
+    if Wrong: display Correct Answer
+(2)Total correct answers
+*/
+
+//User dismiss Alert
+
+//Listen for user to click "continue" button
+
+//Render to next question 
+
+/*
+If loop reaches last question:
+change to OUTRO VIEW
+*/
+
+/*
+OUTRO VIEW: 
+Display Total correct answer
+Listen for user to click "Play Again" button
+*/
+
+//Reset STORE
+//Go back to QUESTION VIEW
